d63fbd773072e2744fc7686e24f6a02844f20b76
albert-alvaro <albert.alvaro@my.jcu.edu.au>
2024-01-05 19:57:49 +0800

    Updated and changed a majority of the files, added inc,class, and asset folders, and removed some fiels which were not needed

--- functions.php
+++ functions.php
@@ -1,23 +1,10 @@
 <?php 
 
-if ( ! function_exists('jedu_theme_support')):
-
-    function jedu_theme_support() {
-        add_theme_support('wp-block-styles');
-        add_editor_style('style.css');
-    }
-endif;
-
-add_action('after_setup_theme', 'jedu_theme_support');
-
-if ( ! function_exists('jedu_styles')):
-function jedu_styles() {
-    echo "testing ";
-    wp_enqueue_style('jedu_style', get_stylesheet_uri(), array(), wp_get_theme()->get('Version'));
-    wp_enqueue_style('jedu_style_block', get_template_directory_uri() . '/assets/css/block.css');
+function jedu_register_styles(){
+    wp_enqueue_style('jedu', get_template_directory_uri() . "/style.css", array(), "1.0.0", 'all');
 }
-endif;
 
-add_action('wp_enqueue', 'jedu_styles')
+add_action('wp_enqueue_scripts', 'jedu_register_styles');
+
 ?>
 
