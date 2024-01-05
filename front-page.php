8245d232220953a2497ca75efc9dda91db616c66
albert-alvaro <albert.alvaro@my.jcu.edu.au>
2024-01-05 20:30:47 +0800

    make the stylesheet and script references dynamic through function.ph

--- front-page.php
+++ front-page.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Blog Site Template">
     <meta name="author" content="https://youtube.com/FollowAndrew">    
-    <link rel="shortcut icon" href="images/logo.png"> 
+    <link rel="shortcut icon" href="assets/img/logo.png"> 
     
     <?php wp_head();?>
 
@@ -17,7 +17,7 @@
 <body>
     
     <header class="header text-center">	    
-	    <a class="site-title pt-lg-4 mb-0" href="index.html">SiteName.dev</a>
+	    <a class="site-title pt-lg-4 mb-0" href="">SiteName.dev</a>
         
 	    <nav class="navbar navbar-expand-lg navbar-dark" >
            
@@ -124,12 +124,8 @@
     
     </div>
     
-    
+    <?php wp_footer();?>
     <!-- Bootstrap Javascript -->          
-	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-	<script src="js/main.js"></script>
 
 </body>
 </html> 
--- functions.php
+++ functions.php
@@ -1,15 +1,21 @@
 <?php 
 
 function jedu_register_styles(){
-    wp_enqueue_style('jedu', get_template_directory_uri() . "/style.css", array('jedu_bootstrap'), "1.0.0", 'all');
-    wp_enqueue_style('jedu_bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), "1.0.0", 'all');
-    wp_enqueue_style('jedu_fonts',"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), "1.0.0", 'all');
-    
-    
-
+    wp_enqueue_style('jedu', get_template_directory_uri() . "/style.css", array('jedu_bootstrap'), wp_get_theme()->get('Version'), 'all');
+    wp_enqueue_style('jedu_bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), "4.4.1", 'all');
+    wp_enqueue_style('jedu_fonts',"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), "5.13.0", 'all');
 }
 
 add_action('wp_enqueue_scripts', 'jedu_register_styles');
+
+function jedu_register_scripts(){
+    wp_enqueue_script('jedu_jquery', 'https://code.jquery.com/jquery-3.4.1.slim.min.js', array(), '3.4.1', true);
+    wp_enqueue_script('jedu_popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', array(), '1.16.0', true);
+    wp_enqueue_script('jedu_bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array(), '3.4.1', true);
+    wp_enqueue_script('jedu_main', get_template_directory_uri() . "assets/js/main.js", array(), '1.0', true);
+}
+
+add_action('wp_enqueue_scripts', 'jedu_register_scripts');
 
 ?>
 
