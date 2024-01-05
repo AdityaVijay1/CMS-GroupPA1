ee057ddf35b6a397d75d0275b064f37d9b05e455
albert-alvaro <albert.alvaro@my.jcu.edu.au>
2024-01-05 22:45:07 +0800

    add wordpress integration, front-page, header, footer now responds dynamically to content edits from wordpress itself

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer class="footer text-center py-2 theme-bg-dark">
+		   
+            <p class="copyright">CP3402 Group 2</p>
+		   
+	    </footer>
+    
+    </div>
+
+    <?php wp_footer();?>
+
+    </body>
+    </html>
--- functions.php
+++ functions.php
@@ -1,5 +1,11 @@
 <?php 
 
+function jedu_theme_support(){
+add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'jedu_theme_support');
+
 function jedu_register_styles(){
     wp_enqueue_style('jedu', get_template_directory_uri() . "/style.css", array('jedu_bootstrap'), wp_get_theme()->get('Version'), 'all');
     wp_enqueue_style('jedu_bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), "4.4.1", 'all');
