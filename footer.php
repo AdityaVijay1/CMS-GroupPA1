fe2e8ccb1df30c819f7b086349d8ee70bd2f3482
albert-alvaro <albert.alvaro@my.jcu.edu.au>
2024-01-06 19:10:16 +0800

    updated footer

--- footer.php
+++ footer.php
@@ -1,10 +1,12 @@
 <footer class="footer text-center py-2 theme-bg-dark">
-		   
-            <p class="copyright">CP3402 Group 2</p>
+
+            <p class="copyright">This Website is powered by<a href="https://wordpress.org/">WordPress</a> Designed By Group PA 1: Aditya Vijay, Albert Alvaro, Tan Beng Siang, Shreya Ranjan</p>
+            <p class="copyright">Copyright &copy; 2024</p>
+
             <?php dynamic_sidebar('footer-1');?>
-		   
-	    </footer>
-    
+
+        </footer>
+
     </div>
 
     <?php wp_footer();?>
