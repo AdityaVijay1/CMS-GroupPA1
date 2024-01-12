1b99270f8e0efe529e65f6706d473dbf6be0369d
AdityaVijay1 <adityavijay1712@gmail.com>
2024-01-12 13:34:14 +0800

    Updates to Header File and a2.html

--- header.php
+++ header.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Blog Site Template">
-    <meta name="author" content="https://youtube.com/FollowAndrew">    
+    <meta name="description" content="James Cook University Open House Content">
+    <meta name="author" content="Aditya Vijay, Albert Alvaro, Tan Beng Siang, Shreya Ranjan">    
     <link rel="shortcut icon" href="/wp-content/themes/jedu/assets/images/logo.png"> 
     
     <?php wp_head();?>
